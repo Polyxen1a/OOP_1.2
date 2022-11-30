eaa7b84327e203a1f4b7d661ae3dfc074d4737be
Polyxen1a <maslowa@inbox.ru>
2022-11-30 21:18:25 +0300

    OOP_1.2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import transport.Car;
+
 public class Main {
     public static void main(String[] args) {
         Car grande = new Car("Lada",
@@ -6,18 +8,14 @@ public class Main {
                 "Жёлтый",
                 2015,
                 "Россия");
-        grande.stamp = "Lada";
-        grande.model = "Grande";
-        grande.yearOfProduction = 2015;
-        grande.countryOfAssembly = "Россия";
-        grande.bodyColor = "Жёлтый";
-        grande.enginCapacity = 1.7;
+
+
 
         System.out.println("Марка автомобиля " + grande.stamp +
                 ", модель " + grande.model +
                 ", год выпуска " + grande.yearOfProduction +
                 ", страна-производитель " + grande.countryOfAssembly +
-                ", цвет кузова " +  grande.bodyColor +
+                ", цвет кузова " + grande.bodyColor +
                 ", объём двигателя " + grande.enginCapacity);
 
         Car audi = new Car("Audi",
@@ -26,18 +24,13 @@ public class Main {
                 "черный",
                 2020,
                 "Германия");
-        audi.stamp = "Audi";
-        audi.model = "A8 50 L TDI quattro";
-        audi.yearOfProduction = 2020;
-        audi.countryOfAssembly = "Германия";
-        audi.bodyColor = "черный";
-        audi.enginCapacity = 3.0;
+        audi.setTransmission("автомат");
 
         System.out.println("Марка автомобиля " + audi.stamp +
                 ", модель " + audi.model +
                 ", год выпуска " + audi.yearOfProduction +
                 ", страна-производитель " + audi.countryOfAssembly +
-                ", цвет кузова " +  audi.bodyColor +
+                ", цвет кузова " + audi.bodyColor +
                 ", объём двигателя " + audi.enginCapacity);
 
         Car bmw = new Car("BMW",
@@ -46,59 +39,18 @@ public class Main {
                 "черный",
                 2021,
                 "Германия");
-        bmw.stamp = "BMW";
-        bmw.model = "Z8";
-        bmw.yearOfProduction = 2021;
-        bmw.countryOfAssembly = "Германия";
-        bmw.bodyColor = "черный";
-        bmw.enginCapacity = 3.0;
 
-        System.out.println("Марка автомобиля " + bmw.stamp +
-                ", модель " + bmw.model +
-                ", год выпуска " + bmw.yearOfProduction +
-                ", страна-производитель " + bmw.countryOfAssembly +
-                ", цвет кузова " +  bmw.bodyColor +
-                ", объём двигателя " + bmw.enginCapacity);
 
-        Car kia = new Car("Kia",
-                "Sportage 4 поколения",
-                2.4,
-                "красный",
-                2018,
-                "Южная Корея");
-        kia.stamp = "Kia";
-        kia.model = "Sportage 4 поколения";
-        kia.yearOfProduction = 2018;
-        kia.countryOfAssembly = "Южная Корея";
-        kia.bodyColor = "красный";
-        kia.enginCapacity = 2.4;
 
-        System.out.println("Марка автомобиля " + kia.stamp +
-                ", модель " + kia.model +
-                ", год выпуска " + kia.yearOfProduction +
-                ", страна-производитель " + kia.countryOfAssembly +
-                ", цвет кузова " +  kia.bodyColor +
-                ", объём двигателя " + kia.enginCapacity);
 
-        Car hyunday = new Car("Hyundai",
-                "Avante",
-                1.6,
-                "оранжевый",
-                2016,
-                "Южная Корея");
-        hyunday.stamp = "Hyundai";
-        hyunday.model = "Avante";
-        hyunday.yearOfProduction = 2016;
-        hyunday.countryOfAssembly = "Южная Корея";
-        hyunday.bodyColor = "оранжевый";
-        hyunday.enginCapacity = 1.6;
-
-        System.out.println("Марка автомобиля " + hyunday.stamp +
-                ", модель " + hyunday.model +
-                ", год выпуска " + hyunday.yearOfProduction +
-                ", страна-производитель " + hyunday.countryOfAssembly +
-                ", цвет кузова " +  hyunday.bodyColor +
-                ", объём двигателя " + hyunday.enginCapacity);
+        private static void printInfo (Car car);
+        System.out.println(
+                car.stamp() + " " + car.model() +
+                        ", год выпуска: " + car.yearOfProduction +
+                        ", страна-производитель " + car.countryOfAssembly +
+                        ", цвет кузова " + car.bodyColor +
+                        ", объём двигателя " + car.enginCapacity);
+        );
 
 
     }
